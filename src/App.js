e56b6b8cb7755af67d92dbd24bb212a7d6be1b2d
Amar <amar2115425@akgec.ac.in>
2024-07-14 06:10:48 +0530

    filter-added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { toast } from "react-toastify";
 const App = () => {
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [category, setCategory]=useState(filterData[0].title);
 
   async function fetchData() {
     setLoading(true);
@@ -34,13 +35,13 @@ const App = () => {
       </div>
      <div className="bg-bgDark2">
      <div>
-        <Filter filterData={filterData} />
+        <Filter filterData={filterData} category={category} setCategory={setCategory} />
       </div>
      </div>
       <div className="w-11/12 max-w-[1200px] 
         mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
       {
-        loading ? <Spinner /> : <Cards courses={courses}/>
+        loading ? <Spinner /> : <Cards courses={courses} category={category} />
       }
       </div>
     </div>
